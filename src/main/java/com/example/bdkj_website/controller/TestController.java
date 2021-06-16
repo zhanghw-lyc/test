491506a81e9894291f0918ff00514b0dc28a53b9
zhanghw-lyc <1289658135@qq.com>
2021-06-16 11:30:07 +0800

    初始化项目

--- src/main/java/com/example/bdkj_website/controller/TestController.java
+++ src/main/java/com/example/bdkj_website/controller/TestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    //aaaaaaaaaaaaa
+    //aaaaaaaaaaaaacccccccccc
     @Autowired
     TestService testService;
 
